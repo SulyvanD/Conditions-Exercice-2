30db3bf7dec65045fee6099458fe622cc1f7369b
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-18 11:55:13 +0100

    Exercice completé

---
+++ Conditions - Exercice 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions___Exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("Vous avez quel age ?");
+            age = int.Parse(Console.ReadLine());
+
+            if (age > 18) {
+                Console.WriteLine($"Vous avez { age } ans, vous êtes donc majeur.");
+            } else {
+                Console.WriteLine($"Vous avez { age } ans, vous êtes donc mineur.");
+            }
+        }
+    }
+}
